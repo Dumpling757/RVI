d5e53bfe71acdd42caf66e14090d15c7f9784a9b
Marius Ulbrich <m.ulbrich@raynet.de>
2021-12-19 17:45:10 +0100

    Impementation of MenuItem ViewModel

---
+++ RayVentoryInstaller/ViewModels/MenuItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace RayVentoryInstaller.ViewModels
+{
+    class MenuItem : HamburgerMenuIconItem
+    {
+        public static readonly DependencyProperty NavigationsDestinationsProperty = DependencyProperty.Register(
+            nameof(NavigationDestination), typeof(Uri), typeof(MenuItem), new PropertyMetadata(default(Uri)));
+
+        public Uri NavigationDestination
+        {
+            get => (Uri)this.GetValue(NavigationsDestinationsProperty);
+            set => this.SetValue(NavigationsDestinationsProperty, value);
+        }
+
+        public static readonly DependencyProperty NavigationTypeProperty = DependencyProperty.Register(
+            nameof(NavigationType), typeof(Type), typeof(MenuItem), new PropertyMetadata(default(Type)));
+
+        public Type NavigationType
+        {
+            get => (Type)this.GetValue(NavigationTypeProperty);
+            set => this.SetValue(NavigationTypeProperty, value);
+        }
+
+        public bool IsNavigation => this.NavigationDestination != null;
+    }
+}
