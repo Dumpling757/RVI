4e0108d0ca2777c1900fc6fdd6e3c580b9420369
Marius Ulbrich <m.ulbrich@raynet.de>
2021-12-19 18:48:20 +0100

    Creation of General Page

---
+++ RayVentoryInstaller/Views/GeneralPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RayVentoryInstaller.Views
+{
+    /// <summary>
+    /// Interaction logic for GeneralPage.xaml
+    /// </summary>
+    public partial class GeneralPage : Page
+    {
+        public GeneralPage()
+        {
+            InitializeComponent();
+        }
+
+        private void tglsw_InstallSQL_Toggled(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tglsw_Instance_Toggled(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void b_VerifySQLConn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
