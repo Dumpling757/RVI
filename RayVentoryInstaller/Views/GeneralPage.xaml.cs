9f9335f3c734184b08bf93fb5b085a8b43a896d7
Marius Ulbrich <m.ulbrich@raynet.de>
2021-12-25 23:39:43 +0100

    Implementation of SQL Permission logic

--- RayVentoryInstaller/Views/GeneralPage.xaml.cs
+++ RayVentoryInstaller/Views/GeneralPage.xaml.cs
@@ -10,6 +10,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.ComponentModel;
+
 
 namespace RayVentoryInstaller.Views
 {
@@ -35,7 +39,77 @@ namespace RayVentoryInstaller.Views
 
         private void b_VerifySQLConn_Click(object sender, RoutedEventArgs e)
         {
+            foreach(Control control in LogicalTreeHelper.GetChildren(this))
+            {
+                if(control is TextBox)
+                {
+                    (control as TextBox).IsEnabled = false;
+                }
+            }
 
+            string connection;
+
+            SqlConnection sqlConnection;
+            SqlCommand sqlCommand;
+            SqlDataReader sqlDataReader;
+
+            string user;
+
+            connection = $"DataSource={ tb_SQLServer.Text}";
+            if(tb_Instance.Text != null)
+            {
+                connection += '\\' + tb_Instance.Text;
+            }
+
+            if(tb_PortName != null)
+            {
+                connection += ':' + tb_PortName.Text;
+            }
+            connection += $";Initial Catalog=master;";
+
+            if(tb_SQLUser != null)
+            {
+                connection += $"User ID={tb_SQLUser.Text};Password={pb_SQLPassword.Password}";
+                user = tb_SQLUser.Text;
+            }
+            else
+            {
+                connection += "Trusted_Connection=True;";
+                user = Environment.UserName;
+            }
+
+            string sql = "SELECT r.name as Role, m.name as Principal FROM master.sys.server_role_members rm" +
+                "INNER JOIN master.sys.server_principals r ON r.principal_id = rm.role_principal_id AND r.type = 'R'" +
+                $" INNER JOIN master.sys.server_principals m ON m.principal_id = rm.member_principal_id WHERE m.name = \'{user}\';";
+
+            sqlConnection = new SqlConnection(connection);
+            try
+            {
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand(sql, sqlConnection);
+                sqlDataReader = sqlCommand.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    if((string)sqlDataReader.GetValue(0) == "Sysadmin")
+                    {
+                        SQLAuthWrong.Visibility = Visibility.Hidden;
+                        SQLAuthRight.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        SQLAuthRight.Visibility = Visibility.Hidden;
+                        SQLAuthWrong.Visibility = Visibility.Visible;
+                    }
+
+                }
+                sqlDataReader.Close();
+                sqlCommand.Dispose();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot open connection!");
+            }
         }
     }
 }
