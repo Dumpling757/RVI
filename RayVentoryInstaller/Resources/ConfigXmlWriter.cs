3d33c37126365987133a62fe0a0e35a79978a7e4
Marius Ulbrich <m.ulbrich@raynet.de>
2021-12-26 20:51:09 +0100

    Creation of ConfigXmlWriter Class

---
+++ RayVentoryInstaller/Resources/ConfigXmlWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Xml;
+using RayVentoryInstaller;
+using RayVentoryInstaller.Views;
+
+namespace RayVentoryInstaller.Resources
+{
+     class ConfigXmlWriter
+    {
+        public ConfigXmlWriter()
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
