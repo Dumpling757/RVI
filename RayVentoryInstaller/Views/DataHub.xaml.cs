851c89c74eff864565f81446a1be78fd2bdad40e
Marius Ulbrich <m.ulbrich@raynet.de>
2021-12-26 14:30:13 +0100

    Creation of other Pages and Insertion of MahApps Declarations

---
+++ RayVentoryInstaller/Views/DataHub.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RayVentoryInstaller.Views
+{
+    /// <summary>
+    /// Interaction logic for DataHub.xaml
+    /// </summary>
+    public partial class DataHub : Page
+    {
+        public DataHub()
+        {
+            InitializeComponent();
+        }
+    }
+}
